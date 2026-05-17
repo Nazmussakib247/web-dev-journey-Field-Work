902a5ed36b2f24ab9cbf8fd68b723a910900b8e8
Nazmus Sakib <nazmuss247@gmail.com>
2026-05-17 19:07:41 +0600

    18

---
+++ phase-04-javascript/day-02-basic-problem-set/problems/18_Day_Finder_Using_Switch.js
@@ -0,0 +1,19 @@
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Saturday");
+        break;
+
+    case 2:
+        console.log("Sunday");
+        break;
+
+    case 3:
+        console.log("Monday");
+        break;
+
+    default:
+        console.log("Invalid Input");
+
+}
