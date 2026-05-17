5ccfd6504962eeea2be510c1653af516c396e0f8
Nazmus Sakib <nazmuss247@gmail.com>
2026-05-17 19:07:48 +0600

    19

---
+++ phase-04-javascript/day-02-basic-problem-set/problems/19_Simple_Calculator.js
@@ -0,0 +1,24 @@
+let number1 = 3829338692379393;
+let number2 = 82489903389;
+let operator = "*";
+
+switch (operator) {
+    case "/":
+        console.log(number1 + number2);
+        break;
+
+    case "-":
+        console.log(number1 - number2);
+        break;
+
+    case "*":
+        console.log(number1 * number2);
+        break;
+
+    case "/":
+        console.log(number1 / number2);
+        break;
+
+    default:
+        console.log("Invalid operator");
+}
